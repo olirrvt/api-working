58eb32fc51b70a23fcf023a101b56c4ff48ad090
Taylor Oliveira <taylorso2004@hotmail.com>
2022-11-28 06:59:18 -0300

    fetch de login

--- src/controllers/usuarios/login.js
+++ src/controllers/usuarios/login.js
@@ -4,13 +4,17 @@ const loginUserController = async (req, res) => {
 
     const usuario = require("../../models/usuarios");
     const { email, senha } = req.body;
+
+
     const usuarioExistente = await usuario.findOne({
         where: { email: email }
     });
+    // Procura o email
 
     if (!usuarioExistente) {
         return res.json({ message: "Verifique seus dados e tente novamente!" })
     };
+    // Compara o email
 
     console.log(usuarioExistente.dataValues);
     const senhaValida = await compare(senha, usuarioExistente.dataValues.senha);
@@ -18,6 +22,8 @@ const loginUserController = async (req, res) => {
     if(!senhaValida) {
         return res.json({ message: "Verifique seus dados e tente novamente!" })
     }
+    // Compara a senha criptografada
+
     await usuario.update({
         id: usuarioExistente.id,
         nome: usuarioExistente.nome,
@@ -25,6 +31,8 @@ const loginUserController = async (req, res) => {
         senha: usuarioExistente.senha,
         logado: 1
     }, { where: { id: usuarioExistente.id } });
+    // Muda o status de logado
+
     const usuarioLogado = await usuario.findOne({ where: { id: usuarioExistente.dataValues.id } });
     return res.json({ message: "Login efetuado com sucesso!", usuario: usuarioLogado })
 }
